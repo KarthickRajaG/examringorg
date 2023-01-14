44de5efbcbc395bef9acd50d84a399f12515e12f
KarthickRajaG <gkarthickrajag@gmail.com>
2023-01-14 14:49:50 +0530

    admin flow added

--- controller/adminController.js
+++ controller/adminController.js
@@ -1,4 +1,17 @@
 const Exam = require('../models/examModel')
+const User = require('../models/userModel')
+const md5 = require('md5')
+
+
+// adminLogin
+exports.adminLogin = async (req,res) => {
+ let found = await User.findOne({email:req.body.email,password:req.body.password})
+ if(found) {
+    res.send({status:true, message:"admin details", data:found})
+ } else {
+    res.send({status:false, message:'admin not found'})
+ }
+}
 
 // addExams
 exports.addExams = async (req,res) => {
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -2,6 +2,7 @@ const router = require("express").Router()
 const {requiresAuth,generateToken} = require("../controller/firebaseController")
 const adminController = require("../controller/adminController")
 
+router.post("/adminLogin",adminController.adminLogin)
 router.post("/addExams",requiresAuth,adminController.addExams)
 
 
